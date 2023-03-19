d03c5a6562c8f8ca75a02b4a0b64ad4cf17b8309
vavellar <victor.avellar01@gmail.com>
2023-03-19 17:06:58 -0300

    classes and objects

---
+++ classes_and_objects/classes_and_objects.rb
@@ -0,0 +1,14 @@
+class Book
+  attr_accessor :title, :author, :pages
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
+end
+
+book1 = Book.new("Harry Potter","JK Rowling", 400)
+puts book1.title
+
+book2 = Book.new("Lord of the rings","Tolkein", 500)
+puts book2.author
